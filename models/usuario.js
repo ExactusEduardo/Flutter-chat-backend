97a6925c7f1146de18110065ac482a06c61b68d6
Eduardo Loo <eduardo.loo@pucp.pe>
2020-09-22 20:38:58 -0500

    Fin sección 8

---
+++ models/usuario.js
@@ -0,0 +1,32 @@
+const { Schema, model } = require('mongoose');
+
+const UsuarioSchema = Schema({
+
+    nombre: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    online: {
+        type: Boolean,
+        default: false
+    }
+
+});
+
+// La función de flecha no modifica el objeto por referencia por eso usamos function() {}
+UsuarioSchema.method('toJSON', function() {
+    const { __v, _id, password, ...object } = this.toObject();
+    object.uid = _id;
+    return object;
+});
+
+module.exports = model('Usuario', UsuarioSchema);
